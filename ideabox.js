55512f830d9a061f6b048d01337577bbd00410e6
Travis Rollins <TROLL59910@aol.com>
2017-05-20 16:13:29 -0600

    Using objects and got votes 90% working

--- ideabox.js
+++ ideabox.js
@@ -1,23 +1,44 @@
 $('.save').on('click', readyToSave)
 
-var cardCounter = 0;
-
 function readyToSave (event) {
   event.preventDefault();
-  cardCounter++;
   var title = $('#title').val();
   var body = $('#body').val();
-  ideabox(title, body);
+  readyToSubmit();
 }
 
-function ideabox(title, body,) {
+function readyToSubmit () {
+  var title = $('#title').val();
+  var body = $('#body').val();
+  var idea = new IdeaBox(title, body);
+  if (title === "" || body === "") {
+    toggleDisabled(true);
+  }
+  else {
+    toggleDisabled(false);
+    addIdea(idea);
+    $('#title').val('');
+    $('#body').val('');
+  }
+}
+
+function IdeaBox (title, body) {
+  this.title = title;
+  this.body = body;
+  console.log(this);
+}
+
+function addIdea(idea) {
+  var ideaTitle = idea.title;
+  var ideaBody = idea.body;
+  i = 0;
   $('#idea-area').prepend(`<div class="idea-card">
-    <h4>${title}</h4>
+    <h4>${ideaTitle}</h4>
     <div class="card-button delete"></div>
-    <p>${body}</p>
+    <p>${ideaBody}</p>
     <div class="card-button vote-up"></div>
     <div class="card-button vote-down"></div>
-    <p class="quality">quality:<span id="quality-check" value="swill">${voteTotal[0]}</span></p>
+    <p class="quality">quality:<span id="quality-check" value="swill">${voteTotal[i]}</span></p>
     <hr />
   </div>`)
   $('#title').val('');
@@ -25,17 +46,44 @@ function ideabox(title, body,) {
   $('h4, p').attr('contenteditable', 'true');
 }
 
+function toggleDisabled (value) {
+  $('.save').attr("disabled", value);
+}
+
+$('input').focus(function () {
+  toggleDisabled(false);
+})
+
+/*Delete button*/
+
 $('#idea-area').on('click', '.delete', removeIdea)
 
 function removeIdea () {
   $(this).parent().remove();
 }
 
+/* Up or down vote */
+
 var voteTotal = [' swill', ' plausible', ' genius'];
 var i = 0;
 
 $('#idea-area').on('click', '.vote-up', function() {
-  this.i = i;
   i++;
+  minMax();
   $(this).parent().find('#quality-check').text(voteTotal[i]);
 })
+
+$('#idea-area').on('click', '.vote-down', function() {
+  i--;
+  minMax();
+  $(this).parent().find('#quality-check').text(voteTotal[i]);
+})
+
+function minMax () {
+  if (i > 2) {
+    i = 2;
+  }
+  if (i < 0) {
+    i = 0;
+  }
+}
